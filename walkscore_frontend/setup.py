bb4a60e2be9ca9561810cdf4bd5611dc4e732abe
James Malone <jamalone@gmail.com>
2015-04-08 23:11:02 -0700

    Initial commit to repo

---
+++ walkscore_frontend/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='walkscore_frontend',
+    version='0.1',
+    description='Front-end (scrape API) for the Walkscore website',
+    url='http://github.com/evilsoapbox',
+    author='James Malone',
+    author_email='jamalone@gmail.com',
+    license='MIT',
+    packages=['walkscore_frontend'],
+    zip_safe=False)
