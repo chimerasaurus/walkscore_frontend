bc86ed178e7cca22693f444133a113c44cf024bb
James Malone <jamalone@gmail.com>
2015-04-11 22:47:19 -0700

    Additions and fixes

--- setup.py
+++ setup.py
@@ -1,15 +1,33 @@
+# Imports for this setup script
 from setuptools import setup
 
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+# Setup metadata
 setup(name='walkscore_frontend',
     version='0.1',
-    description='Front-end (scrape API) for the Walkscore website',
-    url='http://github.com/evilsoapbox',
+    description='Front-end (scraper API) for the Walkscore website',
+    long_description=readme(),
+    url='http://github.com/evilsoapbox/walkscore_frontend',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Scientific/Engineering :: GIS',
+        'Natural Language :: English',
+        'Intended Audience :: Science/Research',
+    ],
+    keywords='walkscore frontend data walk transitscore bikescore',
     author='James Malone',
     author_email='jamalone@gmail.com',
     license='MIT',
     packages=['walkscore_frontend'],
     install_requires=[
         'beautifulsoup4',
-        'requests'
+        'requests',
+        'nose'
     ],
+    include_package_data=True,
     zip_safe=False)
